a4cdccdf7b87ea6042905b9fde39dfe1edfe3976
lenivec <wormsom@gmail.com>
2020-05-15 16:44:40 +0300

    Add ckeditor, plugin youtube and collectstatic

--- django_movie_library/urls.py
+++ django_movie_library/urls.py
@@ -18,8 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('movies.urls')),
 ]
 
