036a944c01897f5f7c7382f2776b17fdbe7eca6a
lenivec <wormsom@gmail.com>
2020-10-03 03:59:07 +0300

    setting up a copy of the original database for testing

--- movies/tests/unit/conftest.py
+++ movies/tests/unit/conftest.py
@@ -1,15 +1,38 @@
 import pytest
 import os
-from django_movie_library import local_settings
+
+from django.db import connections
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
-@pytest.fixture(scope='session')
+def run_sql(sql):
+    conn = psycopg2.connect(
+        database=os.environ.get("movie_lib_db_name"),
+        user=os.environ.get('movie_lib_db_user'),
+        password=os.environ.get('movie_lib_db_password'),
+        host='127.0.0.1',
+        port='5432',
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.close()
+
+
+@pytest.yield_fixture(scope='session')
 def django_db_setup():
-    local_settings.DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('movie_lib_db_name'),
-        'USER': os.environ.get('movie_lib_db_user'),
-        'PASSWORD': os.environ.get('movie_lib_db_password'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    from django.conf import settings
+
+    settings.DATABASES['default']['NAME'] = 'the_copied_db'
+
+    run_sql('DROP DATABASE IF EXISTS the_copied_db')
+    run_sql(f'CREATE DATABASE the_copied_db TEMPLATE {os.environ.get("movie_lib_db_name")}')
+
+    yield
+
+    for connection in connections.all():
+        connection.close()
+
+    run_sql('DROP DATABASE the_copied_db')
--- movies/tests/unit/test_views.py
+++ movies/tests/unit/test_views.py
@@ -18,7 +18,7 @@ from movies.models import Movie, Actor
 @pytest.mark.django_db
 class TestUrlStatusCode:
     """
-    Tests for task urls status code
+    Tests for movies urls status code
     """
     def test_get_base_page_status_code(self, client, page_url):
         page = client.get(page_url)
@@ -30,7 +30,7 @@ class TestUrlStatusCode:
 @pytest.mark.django_db
 class TestUrlStatusCodeForActors:
     """
-    Tests for task actor urls status code
+    Tests for movies actor urls status code
     """
     def test_get_base_page_status_code_for_actors(self, client):
         for actor in Actor.objects.all():
@@ -53,7 +53,7 @@ class TestUrlStatusCodeForActors:
 @pytest.mark.django_db
 class TestsTemplatePresence:
     """
-    Tests for task template presence
+    Tests for movies template presence
     """
     def test_page_is_base_page(self, client, template_name):
         page = client.get("/")
@@ -72,3 +72,18 @@ def test_pages_template_not_from_this_url(client, template_name):
     page = client.get("/")
     assert template_name in (template.name for template in page.templates), \
         f"{template_name} page on the main url, but should not be"
+
+
+# Why it doesn't work?
+# @pytest.mark.parametrize('page_url', [
+#     actor.name for actor in Actor.objects.all()
+# ])
+# @pytest.mark.django_db
+# class TestUrlStatusCodeActorsFromBD:
+#     """
+#     Tests for movies actor urls status code
+#     """
+#     def test_get_base_page_status_code_actors_from_bd(self, client, page_url):
+#         page = client.get(f"/actor/{page_url}/")
+#         assert page.status_code == 200, \
+#             f"'/actor/{page_url}/' status code is not 200, but should be"
